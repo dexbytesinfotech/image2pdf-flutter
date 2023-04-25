f98336ebf14073ed7a3e5dffe9d5f54320cf961c
Dinesh <dineshpal@dexbytes.com>
2023-04-25 15:47:05 +0530

    Code optimize

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,14 +8,19 @@ import 'package:image2pdf_flutter/image_to_pdf.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  /// Init Image PDF it must required to get saved PDF.
   await ImageToPdfMain.initValue();
 
+  /// Set icon and some another configuration setting.
+  /// Set document name text style.
   Configuration.instance!.editDocumentNameTextStyle = Configuration
       .instance!.editDocumentNameTextStyle!
       .copyWith(fontSize: 14, color: Colors.black);
 
+  /// Set camera icon background.
   Configuration.instance!.cameraIconBgColor = Colors.black;
   // Configuration.instance!.cameraIcon = Icon(Icons.add, size: 80);
 
+  /// Open main screen of PDF.
   runApp(ImageToPdfMain(pdfPathCallBack: (String pdfPath) {}));
 }

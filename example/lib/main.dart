5de20424c17dfefe9c88d8e70ee13b4443a939c5
Dinesh <dineshpal@dexbytes.com>
2023-04-25 12:18:45 +0530

    Dart document update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,13 +3,11 @@
 // found in the LICENSE file.
 import 'dart:async';
 
-import 'package:equatable/equatable.dart';
 import 'package:image2pdf_flutter/image_to_pdf.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final e = E();
-  e.b = true;
+
   await ImageToPdfMain.initValue();
 
   Configuration.instance!.editDocumentNameTextStyle = Configuration
@@ -20,12 +18,4 @@ Future<void> main() async {
   // Configuration.instance!.cameraIcon = Icon(Icons.add, size: 80);
 
   runApp(ImageToPdfMain(pdfPathCallBack: (String pdfPath) {}));
-}
-
-/// Test class.
-class E with EquatableMixin {
-  /// Test bool.
-  late bool b;
-  @override
-  List<Object> get props => [b];
 }

d7a21eaa40bbdd29de4c3d286f93a177547a8fdc
Dinesh <dineshpal@dexbytes.com>
2023-05-03 11:50:47 +0530

    Document update

--- lib/image_to_pdf.dart
+++ lib/image_to_pdf.dart
@@ -1,5 +1,5 @@
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/// Use of this source code is governed by a BSD-style license that can be
+/// found in the LICENSE file.
 
 library image2pdf_flutter;
 
